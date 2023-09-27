9dd51d2f049b49ccd1bfcfe11287d7e44fe1a726
knl93 <knlmeshram24@gmail.com>
2023-09-28 01:18:38 +0530

    added requirement.txt and update mysparkcode.py

--- mysparkcode.py
+++ mysparkcode.py
@@ -16,8 +16,4 @@ df=spark.read.format('json').option('mode','dropmalformed').load(data1)
 
 #use explode fun flatten the array into struct.
 res=df.withColumn('theme1name',col('theme1.Name')).withColumn('theme1percent',col('theme1.Percent'))\
-    .drop('theme1').withColumn('theme_namecode',explode(col('theme_namecode'))) \
-    .withColumn('theme2code',col('theme_namecode.code')).withColumn('theme2name',col('theme_namecode.name')) \
-    .drop('theme_namecode')
-
-
+    .drop('theme1')
